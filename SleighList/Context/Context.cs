45acd405457cc30178606e4b37442fb4b18b81cd
Bryan Goncalves <goncalvesbryan424@gmail.com>
2024-12-12 13:22:07 -0300

    Atualizando 13:22

--- SleighList/Context/Context.cs
+++ SleighList/Context/Context.cs
@@ -2,11 +2,35 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SleighList.Models;
 
 namespace SleighList.Context
 {
-    public class Context
+    public class Context : DbContext
     {
-        
+        public Context()
+        {
+        }
+
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+        }
+
+        // OnConfiguring -> Possui a configuracao da conexao com
+        //o banco de dados
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // colocar as informacoes do banco
+            // As configuracoes existem?
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=NOTE36-S28\\SQLEXPRESS; Initial Catalog = Bibliotec_mvc; User Id=sa; Password=123; TrustServerCertificate = true");
+            }
+        }
+
+        public DbSet<Usuario> Usuario { get; set; }
+
+        public DbSet<Item> Item { get; set; }
     }
 }
