93499acff47710c4e02d20e677b156d3875ec416
Bryan Goncalves <goncalvesbryan424@gmail.com>
2024-12-12 16:32:57 -0300

    Iniciando Projeto

--- SleighList/obj/Debug/net8.0/SleighList.AssemblyInfo.cs
+++ SleighList/obj/Debug/net8.0/SleighList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SleighList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c6dd9fc0c151a04f83f27f26e10cdf5042d9b6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e55118d1389c886630f96529a5faef60b970963")]
 [assembly: System.Reflection.AssemblyProductAttribute("SleighList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SleighList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
